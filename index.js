d7359c484eec217d1197a0a866ebc0319e9c100e
cndgus6413 <cndgus6413@naver.com>
2022-07-05 18:16:25 +0900

    Routing

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const app = express();
 
-app.use(() => {
-    console.log("WE GOT A NEW REQUEST")
+// app.use((req, res) => {
+//     console.log("WE GOT A NEW REQUEST")
+//     res.send("HELLO")
+// })
+app.get('/', (req, res) => {
+    res.send("HOME")
+})
+
+app.post('/help', (req, res) => {
+    res.send("Post /help")
+})
+
+app.get('/help', (req, res) => {
+    res.send("HELP!")
+})
+
+app.get('/about', (req, res) => {
+    res.send("About")
+})
+
+app.get('*', (req, res) => {
+    res.send("No Where")
 })
 
 app.listen(3000, () => {
