9ed7c56199e6106731d0fa7c4e0a41ef3de48534
cndgus6413 <cndgus6413@naver.com>
2022-07-05 17:54:17 +0900

    express

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const app = express()
+const app = express();
+
+app.use(() => {
+    console.log("WE GOT A NEW REQUEST")
+})
 
 app.listen(3000, () => {
     console.log("LISTENING ON PORT 3000")
